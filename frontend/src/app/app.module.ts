df852695735abbe4c65f9063fc6c75fbf3cb662b
yisus82 <djalma8@gmail.com>
2019-04-14 17:59:12 +0200

    Moved site layout components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { SiteLayoutHeaderComponent } from './components/site-layout-header/site-layout-header.component';
+import { SiteLayoutFooterComponent } from './components/site-layout-footer/site-layout-footer.component';
+import { SiteLayoutNavbarComponent } from './components/site-layout-navbar/site-layout-navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    SiteLayoutHeaderComponent,
+    SiteLayoutFooterComponent,
+    SiteLayoutNavbarComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
